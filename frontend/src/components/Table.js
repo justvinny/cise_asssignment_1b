ac62b409659aafd0d75413d8e86493c169060e5c
Jose Santos <67150374+jose-vs@users.noreply.github.com>
2022-05-18 23:06:19 +1200

    added popup for summary

--- frontend/src/components/Table.js
+++ frontend/src/components/Table.js
@@ -1,16 +1,26 @@
 import * as React from "react";
-import Paper from "@mui/material/Paper";
-import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
-import TableCell from "@mui/material/TableCell";
-import TableContainer from "@mui/material/TableContainer";
-import TableHead from "@mui/material/TableHead";
-import TablePagination from "@mui/material/TablePagination";
-import TableRow from "@mui/material/TableRow";
+import {
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TablePagination,
+  TableRow,
+  Box,
+  Typography,
+  Modal,
+} from "@mui/material";
 
 const ArticleTable = ({ data, columns }) => {
   const [page, setPage] = React.useState(0);
   const [rowsPerPage, setRowsPerPage] = React.useState(10);
+  const [open, setModalOpen] = React.useState(false);
+
+  const [selectedArticle, setSelectedArticle] = React.useState(null);
+
+  const ref = React.useRef();
 
   const handleChangePage = (newPage) => {
     setPage(newPage);
@@ -19,6 +29,11 @@ const ArticleTable = ({ data, columns }) => {
   const handleChangeRowsPerPage = (event) => {
     setRowsPerPage(event.target.value);
     setPage(0);
+  };
+
+  const handleOnModalOpen = (row) => {
+    setModalOpen(true);
+    setSelectedArticle(row);
   };
 
   return (
@@ -39,11 +54,32 @@ const ArticleTable = ({ data, columns }) => {
             </TableRow>
           </TableHead>
           <TableBody>
+            <Modal
+              open={open}
+              onClose={() => setModalOpen(false)}
+              aria-labelledby="modal-modal-title"
+              aria-describedby="modal-modal-description"
+            >
+              {selectedArticle ? (
+                <ArticleSummary
+                  title={selectedArticle.title}
+                  authors={selectedArticle.authors}
+                />
+              ) : (
+                <div>nothing to show</div>
+              )}
+            </Modal>
             {data
               .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
               .map((row, idx) => {
                 return (
-                  <TableRow hover role="checkbox" tabIndex={-1} key={idx}>
+                  <TableRow
+                    hover
+                    role="checkbox"
+                    tabIndex={-1}
+                    key={idx}
+                    onClick={() => handleOnModalOpen(row)}
+                  >
                     {columns.map((column) => {
                       const value = row[column.id];
                       return (
@@ -71,6 +107,29 @@ const ArticleTable = ({ data, columns }) => {
       />
     </Paper>
   );
+};
+
+const ArticleSummary = ({ title, authors }) => {
+  return (
+    <Box sx={popupStyle}>
+      <Typography id="modal-modal-title" variant="h6" component="h2">
+        {title}
+      </Typography>
+      <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+        {authors}
+      </Typography>
+    </Box>
+  );
+};
+
+const popupStyle = {
+  position: "absolute",
+  top: "30%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  p: 4,
 };
 
 export default ArticleTable;
